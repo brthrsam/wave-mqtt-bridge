810f6ede57b8590ddb548504e01f2bf1b374e3b9
brthrsam <isampo@me.com>
2020-12-07 14:09:16 +0200

    Update waveplus.py

--- src/waveplus.py
+++ src/waveplus.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2018 Airthings AS
+# Copyright (c) 2019 Hexagon <Hexagon@GitHub>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,138 +19,185 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#
-# https://airthings.com
 
-# ===============================
-# Module import dependencies
-# ===============================
 
-from bluepy.btle import UUID, Peripheral, Scanner, DefaultDelegate
+import wave
+import waveplus
+
+from datetime import datetime
+import paho.mqtt.publish as publish
+import logging
+import os
 import sys
 import time
-import struct
 
-# ====================================
-# Utility functions for WavePlus class
-# ====================================
+# Read environment
+mqtt_host = os.getenv('MQTT_HOST')
+mqtt_port = os.getenv('MQTT_PORT', 1883)
+mqtt_user = os.getenv('MQTT_USER')
+mqtt_pass = os.getenv('MQTT_PASS')
+mqtt_topic_lt = os.getenv('MQTT_TOPIC_RADON_LT')
+mqtt_topic_st = os.getenv('MQTT_TOPIC_RADON_ST')
+mqtt_topic_t = os.getenv('MQTT_TOPIC_TEMPERATURE')
+mqtt_topic_h = os.getenv('MQTT_TOPIC_HUMIDITY')
+mqtt_topic_p = os.getenv('MQTT_TOPIC_PRESSURE')
+mqtt_topic_c = os.getenv('MQTT_TOPIC_CO2')
+mqtt_topic_v = os.getenv('MQTT_TOPIC_VOC')
+aw_serial = os.getenv('AW_SERIAL')
+aw_type = os.getenv('AW_TYPE')
+wavemon_logfile = os.getenv('WM_LOGFILE')
+wavemon_interval_s = os.getenv('WM_INTERVAL_S')
 
-def parseSerialNumber(ManuDataHexStr):
-    if (ManuDataHexStr == "None"):
-        SN = "Unknown"
-    else:
-        ManuData = bytearray.fromhex(ManuDataHexStr)
+# Set up logging
+if wavemon_logfile != "None":
+    logging.basicConfig(level=logging.DEBUG, filename=wavemon_logfile, filemode='w', format='%(name)s - %(asctime)s - %(levelname)s - %(message)s')
+else:
+    logging.basicConfig(level=logging.DEBUG)
 
-        if (((ManuData[1] << 8) | ManuData[0]) == 0x0334):
-            SN  =  ManuData[2]
-            SN |= (ManuData[3] << 8)
-            SN |= (ManuData[4] << 16)
-            SN |= (ManuData[5] << 24)
-        else:
-            SN = "Unknown"
-    return SN
+# Log environment
+logging.info("Config: MQTT_HOST resolved to %s", mqtt_host)
+logging.info("Config: WM_LOGFILE resolved to %s", wavemon_logfile)
+logging.info("Config: WM_INTERVAL_S resolved to %s", wavemon_interval_s)
 
-# ===============================
-# Class WavePlus
-# ===============================
+# Check environment variables
+if wavemon_interval_s == None:
+    logging.info("WM_INTERVAL_S defaulted to 300s (5m)");
+    wavemon_interval_s = "300"
 
-class WavePlus():
+if wavemon_interval_s.isdigit() is not True:
+    logging.info("WM_INTERVAL_S must be numerical");
+    sys.exit(1)
+else:
+    wavemon_interval_s = int(wavemon_interval_s)
 
+if wavemon_interval_s < 30:
+    logging.info("WM_INTERVAL_S cannot be lower than 30s");
+    sys.exit(1)
+
+if aw_serial == None:
+    logging.info("Missing AW_SERIAL");
+    sys.exit(1)
+
+if len(aw_serial) < 3:
+    logging.info("Missing AW_SERIAL");
+    sys.exit(1)
+
+if aw_serial.isdigit() is not True or len(aw_serial) != 10:
+    logging.info("AW_SERIAL has invalid format");
+    sys.exit(1)
+
+if aw_type != "WAVE" and aw_type != "WAVEPLUS":
+    logging.error("AW_TYPE defaulting to WAVE")
+    aw_TYPE = "WAVE"
+
+# Log wave
+logging.info("Config: AW_SERIAL resolved to %s", aw_serial)
+logging.info("Config: AW_TYPE resolved to %s", aw_type)
+
+if mqtt_host != None:
+    logging.info("MQTT publishing enabled")
+
+    # Log extended environment for MQTT
+    logging.info("Config: MQTT_PORT resolved to %s", mqtt_port)
+    logging.info("Config: MQTT_USER resolved to %s", mqtt_user)
+    # logging.info("Config: MQTT_PASS resolved to ", mqtt_pass)
+    logging.info("Config: MQTT_TOPIC_RADON_LT resolved to %s", mqtt_topic_lt)
+    logging.info("Config: MQTT_TOPIC_RADON_ST resolved to %s", mqtt_topic_st)
+    logging.info("Config: MQTT_TOPIC_TEMPERATURE resolved to %s", mqtt_topic_t)
+    logging.info("Config: MQTT_TOPIC_HUMIDITY resolved to %s", mqtt_topic_h)
+    logging.info("Config: MQTT_TOPIC_PRESSURE resolved to %s", mqtt_topic_p)
+    logging.info("Config: MQTT_TOPIC_CO2 resolved to %s", mqtt_topic_c)
+    logging.info("Config: MQTT_TOPIC_VOC resolved to %s", mqtt_topic_v)
+
+def publish_mqtt(w_st,w_lt,w_temp,w_humidity,w_pressure,w_co2,w_voc):
     
-    
-    def __init__(self, SerialNumber):
-        self.periph        = None
-        self.curr_val_char = None
-        self.MacAddr       = None
-        self.SN            = SerialNumber
-        self.uuid          = UUID("b42e2a68-ade7-11e4-89d3-123b93f75cba")
+    mqtt_msgs = []
+    mqtt_auth = None
+    if mqtt_user != None:
+        mqtt_auth = {'username':mqtt_user, 'password':mqtt_pass}
+    if mqtt_topic_lt != None:
+        mqtt_msgs.append((mqtt_topic_lt,w_lt,1,False))
+    if mqtt_topic_st != None:
+        mqtt_msgs.append((mqtt_topic_st,w_st,1,False))
+    if mqtt_topic_t != None:
+        mqtt_msgs.append((mqtt_topic_t,w_temp,1,False))
+    if mqtt_topic_h != None:
+        mqtt_msgs.append((mqtt_topic_h,w_humidity,1,False))
+    if mqtt_topic_p != None:
+        mqtt_msgs.append((mqtt_topic_p,w_pressure,1,False))
+    if mqtt_topic_c != None:
+        mqtt_msgs.append((mqtt_topic_c,w_co2,1,False))
+    if mqtt_topic_v != None:
+        mqtt_msgs.append((mqtt_topic_v,w_voc,1,False))
+    try:
+        publish.multiple(mqtt_msgs, hostname=mqtt_host, port=mqtt_port, client_id="wavemqttbridge", auth=mqtt_auth, tls=None)
+    except Exception as ex:
+        logging.error('Exception while publishing to MQTT broker: {}'.format(ex))
 
-    def connect(self):
-        # Auto-discover device on first connection
-        if (self.MacAddr is None):
-            scanner     = Scanner().withDelegate(DefaultDelegate())
-            searchCount = 0
-            while self.MacAddr is None and searchCount < 50:
-                devices      = scanner.scan(0.1) # 0.1 seconds scan period
-                searchCount += 1
-                for dev in devices:
-                    ManuData = dev.getValueText(255)
-                    SN = parseSerialNumber(ManuData)
-                    if (SN == self.SN):
-                        self.MacAddr = dev.addr # exits the while loop on next conditional check
-                        break # exit for loop
+while True:
+    
+    start = datetime.now()
+
+    # Use regular Wave
+    if aw_type == "WAVE":
+        try:
+            w = wave.Wave(aw_serial)
+            w.connect()
+
+            date_time    = w.read(wave.SENSOR_IDX_DATETIME)
+            humidity     = w.read(wave.SENSOR_IDX_HUMIDITY)
+            temperature  = w.read(wave.SENSOR_IDX_TEMPERATURE)
+            radon_st_avg = w.read(wave.SENSOR_IDX_RADON_ST_AVG)
+            radon_lt_avg = w.read(wave.SENSOR_IDX_RADON_LT_AVG)
+
+            if mqtt_host != None:
+                publish_mqtt(radon_st_avg,radon_lt_avg,temperature,humidity,None,None,None)
+
+        except Exception as ex:
+            logging.error('Exception while communicating with wave: {}'.format(ex))
+
+        finally:
+            w.disconnect()
+
+
+    # User Wave Plus
+    if aw_type == "WAVEPLUS": 
+        try:
+            wp = waveplus.WavePlus(int(aw_serial))
+            wp.connect()
             
-            if (self.MacAddr is None):
-                print "ERROR: Could not find device."
-                print "GUIDE: (1) Please verify the serial number."
-                print "       (2) Ensure that the device is advertising."
-                print "       (3) Retry connection."
-                sys.exit(1)
-        
-        # Connect to device
-        if (self.periph is None):
-            self.periph = Peripheral(self.MacAddr)
-        if (self.curr_val_char is None):
-            self.curr_val_char = self.periph.getCharacteristics(uuid=self.uuid)[0]
-        
-    def read(self):
-        if (self.curr_val_char is None):
-            print "ERROR: Devices are not connected."
-            sys.exit(1)            
-        rawdata = self.curr_val_char.read()
-        rawdata = struct.unpack('BBBBHHHHHHHH', rawdata)
-        sensors = Sensors()
-        sensors.set(rawdata)
-        return sensors
-    
-    def disconnect(self):
-        if self.periph is not None:
-            self.periph.disconnect()
-            self.periph = None
-            self.curr_val_char = None
+            # read values
+            sensors = wp.read()
+            
+            # extract
+            date_time    = datetime.now()
+            humidity     = sensors.getValue(waveplus.SENSOR_IDX_HUMIDITY)
+            radon_st_avg = sensors.getValue(waveplus.SENSOR_IDX_RADON_SHORT_TERM_AVG)
+            radon_lt_avg = sensors.getValue(waveplus.SENSOR_IDX_RADON_LONG_TERM_AVG)
+            temperature  = sensors.getValue(waveplus.SENSOR_IDX_TEMPERATURE)
+            pressure     = sensors.getValue(waveplus.SENSOR_IDX_REL_ATM_PRESSURE)
+            CO2_lvl      = sensors.getValue(waveplus.SENSOR_IDX_CO2_LVL)
+            VOC_lvl      = sensors.getValue(waveplus.SENSOR_IDX_VOC_LVL)
+            
+            # Print data
+            if mqtt_host != None:
+                publish_mqtt(radon_st_avg,radon_lt_avg,temperature,humidity,pressure,CO2_lvl,VOC_lvl)
 
-# ===================================
-# Class Sensor and sensor definitions
-# ===================================
+        except Exception as ex:
+            logging.error('Exception while communicating with wave plus: {}'.format(ex))
+                        
+        finally:
+            wp.disconnect()
 
-NUMBER_OF_SENSORS               = 7
-SENSOR_IDX_HUMIDITY             = 0
-SENSOR_IDX_RADON_SHORT_TERM_AVG = 1
-SENSOR_IDX_RADON_LONG_TERM_AVG  = 2
-SENSOR_IDX_TEMPERATURE          = 3
-SENSOR_IDX_REL_ATM_PRESSURE     = 4
-SENSOR_IDX_CO2_LVL              = 5
-SENSOR_IDX_VOC_LVL              = 6
 
-class Sensors():
-    def __init__(self):
-        self.sensor_version = None
-        self.sensor_data    = [None]*NUMBER_OF_SENSORS
-        self.sensor_units   = ["%rH", "Bq/m3", "Bq/m3", "degC", "hPa", "ppm", "ppb"]
-    
-    def set(self, rawData):
-        self.sensor_version = rawData[0]
-        if (self.sensor_version == 1):
-            self.sensor_data[SENSOR_IDX_HUMIDITY]             = rawData[1]/2.0
-            self.sensor_data[SENSOR_IDX_RADON_SHORT_TERM_AVG] = self.conv2radon(rawData[4])
-            self.sensor_data[SENSOR_IDX_RADON_LONG_TERM_AVG]  = self.conv2radon(rawData[5])
-            self.sensor_data[SENSOR_IDX_TEMPERATURE]          = rawData[6]/100.0
-            self.sensor_data[SENSOR_IDX_REL_ATM_PRESSURE]     = rawData[7]/50.0
-            self.sensor_data[SENSOR_IDX_CO2_LVL]              = rawData[8]*1.0
-            self.sensor_data[SENSOR_IDX_VOC_LVL]              = rawData[9]*1.0
-        else:
-            print "ERROR: Unknown sensor version.\n"
-            print "GUIDE: Contact Airthings for support.\n"
-            sys.exit(1)
-   
-    def conv2radon(self, radon_raw):
-        radon = "N/A" # Either invalid measurement, or not available
-        if 0 <= radon_raw <= 16383:
-            radon  = radon_raw
-        return radon
+    # Calculate how long time to wait until next round
+    diff = datetime.now() - start
+    elapsed_ms = (diff.days * 86400000) + (diff.seconds * 1000) + (diff.microseconds / 1000)
+    sleep_s = (wavemon_interval_s)-(elapsed_ms/1000)
 
-    def getValue(self, sensor_index):
-        return self.sensor_data[sensor_index]
+    # Never sleep less than 10 seconds between rounds
+    if sleep_s < 10:
+        sleep_s = 10
 
-    def getUnit(self, sensor_index):
-        return self.sensor_units[sensor_index]
+    logging.info('Round done, sleeping %fs until next interval.', sleep_s) 
+    time.sleep( sleep_s )
